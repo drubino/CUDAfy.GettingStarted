189b16cee0d65d3b0b0e870687a518f7b6d60a7b
rubinod <rubinod@OT-RUBINOD>
2016-05-10 13:49:40 -0400

    Initial Commit

---
+++ CUDAfy.GettingStarted/Kernel.cs
@@ -0,0 +1,29 @@
+﻿using Cudafy;
+using Cudafy.Host;
+using Cudafy.Translator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUDAfy.GettingStarted
+{
+    public class Kernel
+    {
+        public static void Execute()
+        {
+            CudafyModule km = CudafyTranslator.Cudafy();
+            GPGPU gpu = CudafyHost.GetDevice(CudafyModes.Target, CudafyModes.DeviceId);
+            gpu.LoadModule(km);
+            gpu.Launch().TheKernel(); // or gpu.Launch(1, 1, "kernel");
+            Console.WriteLine("Hello, World!");
+        }
+
+        [Cudafy]
+        public static void TheKernel()
+        {
+
+        }
+    }
+}
